e84e88331e1ae305b1cbd3614b46fdfe6eb264a7
Claire Steinhoff <claire.steinhoff@gmail.com>
2022-09-29 12:23:04 -0600

    commit tree 3d17ff4342af53c3f013f1d1684256a688187b4c

--- index.js
+++ index.js
@@ -1,1 +1,33 @@
 // code your solution here
+const record = [
+    { year: "2015", result: "W"},
+    { year: "2014", result: "N/A"},
+    { year: "2013", result: "L"},
+    { year: "2014", result: "N/A"}
+]
+
+// function superbowlWin(element){
+//     if (element.result === "W"){
+//         console.log(element.year)
+//         return element.year;
+//     }
+// }
+
+// console.log(superbowlWin(record))
+// console.log(record.find(superbowlWin))
+
+
+//Writing it below as a function which **includes** .find, but cannot
+//be called actually be used in .find on the array (which was the thrust of the teaching)
+
+function superbowlWin(array) {
+    const champ = array.find((x) => x.result === "W")
+    console.log(champ)
+    if(champ) {
+        return champ.year
+    }
+    return
+}
+
+
+console.log(superbowlWin(record))
